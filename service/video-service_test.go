6391b0d314b48fcf2ef2697651008f3d3a8498ce
Giri Putra Adhittana <girput01@icloud.com>
2022-02-27 20:00:15 +0700

    Add test

---
+++ service/video-service_test.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/giriadhittana01/golang-gin-poc/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	TITLE       = "Video Title"
+	DESCRIPTION = "Video Description"
+	URL         = "https://youtu.be/JgW-i2QjgHQ"
+)
+
+func getVideo() entity.Video {
+	return entity.Video{
+		Title:       TITLE,
+		Description: DESCRIPTION,
+		URL:         URL,
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	service := New()
+
+	service.Save(getVideo())
+
+	videos := service.FindAll()
+
+	firstVideo := videos[0]
+	assert.Nil(t, videos)
+	assert.Equal(t, TITLE, firstVideo.Title)
+	assert.Equal(t, DESCRIPTION, firstVideo.Description)
+	assert.Equal(t, URL, firstVideo.URL)
+}
